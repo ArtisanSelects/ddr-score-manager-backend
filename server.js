87269a5fd9a202122545a5440ddb43c55cfaef57
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2021-10-20 15:39:15 -0400

    Updated cors to not use localhost

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ import requireAuth from "./api/authMiddleware.js";
 const app = express();
 app.use(helmet());
 app.use(compression());
-//origin: localhost:300 and shit in production, no good probably! [TODO]
-app.use(cors({
-    credentials: true,
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 app.use(express.json({limit: '16mb'}));
 app.use(cookieParser());
 app.use(requireAuth);
